083218695f28b7ae91c29610b9cf514dac76eaf4
parkgunwook1 <pkw85428@naver.com>
2024-10-28 21:28:23 +0900

    Entitiy 클래스 Join 문제 해결

--- dev/src/main/java/com/toyproject/hello/dev/comment/entity/Comment.java
+++ dev/src/main/java/com/toyproject/hello/dev/comment/entity/Comment.java
@@ -1,19 +1,28 @@
 package com.toyproject.hello.dev.comment.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import com.toyproject.hello.dev.post.entity.Post;
+import jakarta.persistence.*;
 
 import java.sql.Date;
 
 @Entity
+@Table(name = "COMMENT")
 public class Comment {
 
     @Id
-    public String commentId;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    public int commentId;
 
-    public int postId;
+    @ManyToOne // 다대 일 관계 -> 여러개의 Comment가 하나의 Post에 속할 수 있다는 의미
+    @JoinColumn(name = "post_id", nullable = false) // 외래키 설정
+    public Post post;
     public String author;
     public String content;
+
+    @Column(name = "create_date")
     public Date createDate;
+
+    @Column(name = "modified_date")
     public Date modifiedDate;
 }
--- dev/src/main/java/com/toyproject/hello/dev/post/entity/Post.java
+++ dev/src/main/java/com/toyproject/hello/dev/post/entity/Post.java
@@ -1,14 +1,18 @@
 package com.toyproject.hello.dev.post.entity;
 
+import com.toyproject.hello.dev.comment.entity.Comment;
 import jakarta.persistence.*;
 
 import java.sql.Date;
+import java.util.List;
 
 @Entity
+@Table(name = "POST")
 public class Post {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
     public int postId;
 
     @Column(nullable = false, length = 50)
@@ -20,9 +24,17 @@ public class Post {
     @Column(nullable = false)
     public String author;
 
+    @Column(name = "create_date")
     public Date createDate;
+
+    @Column(name = "modified_date")
     public Date modifiedDate;
+
+    @Column(name = "view_count")
     public int viewCount;
     public String image;
-    public String comments;
+
+    // 댓글과의 일대다 관계
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    public List<Comment> comments;
 }
