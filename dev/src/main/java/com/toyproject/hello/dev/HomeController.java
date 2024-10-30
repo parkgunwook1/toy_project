173d154f2d16889edc3c6c996cf59f80077a3895
parkgunwook1 <pkw85428@naver.com>
2024-10-30 23:21:00 +0900

    post dev..ing

--- dev/src/main/java/com/toyproject/hello/dev/HomeController.java
+++ dev/src/main/java/com/toyproject/hello/dev/HomeController.java
@@ -1,15 +1,27 @@
 package com.toyproject.hello.dev;
 
+import com.toyproject.hello.dev.post.dto.PostDto;
+import com.toyproject.hello.dev.post.entity.Post;
+import com.toyproject.hello.dev.post.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor // lombok 제공 : final 붙은 속성 생성자 자동으로 만들어준다.
 public class HomeController {
 
+    @Autowired
+    private final PostService postService;
 
     @GetMapping("/")
-    public String home() {
-
+    public String home(Model model) {
+//        List<PostDto> posts = postService.getPostList();
+//        model.addAttribute("posts" , posts);
         return "index";
     }
 }
--- dev/src/main/java/com/toyproject/hello/dev/post/entity/Post.java
+++ dev/src/main/java/com/toyproject/hello/dev/post/entity/Post.java
@@ -1,13 +1,20 @@
 package com.toyproject.hello.dev.post.entity;
 
 import com.toyproject.hello.dev.comment.entity.Comment;
+import com.toyproject.hello.dev.post.dto.PostDto;
 import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.sql.Date;
 import java.util.List;
 
 @Entity
 @Table(name = "POST")
+@Getter
+@Setter
+@Builder
 public class Post {
 
     @Id
@@ -32,10 +39,30 @@ public class Post {
 
     @Column(name = "view_count")
     public int viewCount;
+
     @Column(length = 300)
     public String image;
+
+    @Column(nullable = false)
+    public String status;
 
     // 댓글과의 일대다 관계
     @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
     public List<Comment> comments;
+
+    // DTO 변환 메서드
+    public static Post ToPostEntity(PostDto postDto) {
+        return Post.builder()
+                .postId(postDto.getPostId())
+                .title(postDto.getTitle())
+                .content(postDto.getContent())
+                .author(postDto.getAuthor())
+                .createDate(postDto.getCreateDate())
+                .modifiedDate(postDto.getModifiedDate())
+                .viewCount(postDto.getViewCount())
+                .image(postDto.getImage())
+                .status(postDto.getStatus())
+                .comments(postDto.getComments())
+                .build();
+    }
 }
